969b8e21edc967a0c8c0c32487820751e83e5e88
nidaKorsan <32648694+nidaKorsan@users.noreply.github.com>
2019-07-10 15:48:11 +0300

    Add files via upload

---
+++ LilGame/hello.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from pygame.locals import *
+
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Space Invaders")
+
+
+notOver = True
+
+
+print("Hello world")
+
+# PROJECT IDEA: SPACE INVADERS
+# GAME TYPE = 2D, ARCADE GOD
+# GAME WILL INCLUDE TILES
+# SINGLE PLAYER GAME
+
+# PLAN:
+# CREATE SCREEN
+# CREATE THE CANNON
+# MOVE THE CANNON
+# CREATE ALIEN
+# MOVE THE ALIEN
+# CREATE CANNON BALL
+# SHOOT THE ALIEN
+# CREATE ALLIANCE
+# COVERS FOR CANNON
+# ALIENS WITH GUNS
+# NEW LEVELS
+# MAIN MENU
+# SOUNDS
---
+++ LilGame/main.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+from pygame.locals import *
+from Game import Game
+
+game = Game()
+game.run()
+print("Hello world")
+
+# PROJECT IDEA: SPACE INVADERS
+# GAME TYPE = 2D, ARCADE GOD
+# GAME WILL INCLUDE TILES
+# SINGLE PLAYER GAME
+
+# PLAN:
+# CREATE SCREEN                 done
+# CREATE GAME CLASS(C)          done
+# CREATE THE CANNON             done
+# MOVE THE CANNON               done
+# CREATE ALIEN                  done
+# MOVE THE ALIEN                done
+# CREATE CANNON BALL            done
+# CREATE ALLIANCE               done
+# SHOOT THE ALIEN               done
+# COVERS FOR CANNON             done
+# MOVING ALLIANCE               done
+# ALIENS WITH GUNS              done
+# NEW LEVELS
+# MAIN MENU
+# SOUNDS
